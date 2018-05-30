79fb3b01ba2a568a458988bc09e5119e5859141a
Ignacio911 <Ignacio911@gmail.com>
2018-05-30 07:07:08 +0000

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# https://github.com/Ignacio911/assetssju8.pythonanywhere.com/blob/master/sitemap.xml.gz?raw=true
+# https://assetssju8.pythonanywhere.com/sitemap.xml.gz
---
+++ pinglist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetssju8.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetssju8.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetssju8.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetssju8.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetssju8.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetssju8.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetssju8.netlify.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetssju8.netlify.com/sitemap.xml.gz
